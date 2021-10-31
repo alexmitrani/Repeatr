68647f13271899970ec8f9ee01a8397519161ee7
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-10-31 13:55:43 -0300

    Relocated choice modelling code in function Repeatr4.R.  Put model output processing code in Repeatr5.R

--- R/Repeatr4.R
+++ R/Repeatr4.R
@@ -1,5 +1,5 @@
 #' @name Repeatr4
-#' @title Produces results using a preferred choice model supplied by the user as "mymodel"
+#' @title Runs choice models
 #' @description
 #' @description
 #' @description
@@ -13,177 +13,122 @@
 #' @import knitr
 #'
 #'
-#' @param mymodel
+#' @param mydf
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' Repeatr4 <- Repeatr4(mydf = Repeatr3)
 #'
 #'
 #'
 #'
-Repeatr4 <- function(mymodel = NULL) {
+#'
 
-  # Report results of the choice modelling for the preferred choice model ----------------------------------
+Repeatr4 <- function(mydf = NULL) {
 
-  summary.mymodel <- summary(summary.mymodel)
+  # Choice modelling --------------------------------
 
-  results.mymodel <- as.data.frame(summary.mymodel[["CoefTable"]])
+  if(file.exists("data.RData")) {
 
-  results.mymodel <- results.mymodel %>%
-    mutate(parameter_id = row_number()) %>%
-    relocate(parameter_id)
+    load("data.RData")
 
-  variable <- row.names(results.mymodel)
+  }
 
-  choice_model_results_table <- cbind(variable, results.mymodel)
+  if (is.null(mydf)==FALSE) {
 
-  choice_model_results_table <- choice_model_results_table %>%
-    mutate(songid = ifelse(parameter_id<=91,parameter_id+1,NA))
+    Repeatr3 <- mydf
 
-  choice_model_results_table <- choice_model_results_table %>%
-    left_join(mysongidlookup)
-
-  choice_model_results_table <- choice_model_results_table %>%
-    mutate(variable = ifelse(parameter_id<=91,song,variable))
-
-  choice_model_results_table$songid <- NULL
-  choice_model_results_table$song <- NULL
-
-  knitr::kable(choice_model_results_table, "pipe")
-
-  write.csv(choice_model_results_table, "fugazi_song_choice_model.csv")
-
-  results.mymodel <- results.mymodel %>%
-    filter(parameter_id<=91)
-
-  results.mymodel <- results.mymodel %>%
-    mutate(songid = parameter_id+1)
-
-  results.mymodel <- results.mymodel %>%
-    left_join(mysongidlookup)
-
-  results.mymodel <- results.mymodel %>%
-    select(songid, song, Estimate, "z-value")
-
-  # to add back in "waiting room" which was the omitted constant in the choice model and has a parameter value of zero by definition.
-
-  results.mymodel.os <- mysongidlookup %>%
-    filter(songid==1) %>%
-    mutate(Estimate = 0) %>%
-    mutate("z-value" = NA)
-
-  results.mymodel <- rbind(results.mymodel, results.mymodel.os)
-
-  results.mymodel <- results.mymodel %>%
-    arrange(desc(Estimate))
-
-  write.csv(results.mymodel, "fugazi_song_preferences.csv")
+  }
 
 
-  # To produce normalised ratings on the interval [0,1] ------------------------
+  Repeatr3$case <- factor(as.numeric(as.factor(Repeatr3$case)))
+  Repeatr3$alt <- as.factor(Repeatr3$alt)
+  Repeatr3$choice <- as.logical(Repeatr3$choice)
+  Repeatr3 <- dfidx(Repeatr3, idx = c("case", "alt"), drop.index = FALSE)
 
-  mydf <- read.csv("fugazi_song_preferences.csv")
+  # The basic model.
 
-  mydf <- mydf %>%
-    rename(rank_rating = X)
+  ml.Repeatr31 <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
+                         + yearsold_6 + yearsold_7 + yearsold_8 , data = Repeatr3)
 
-  mydf <- mydf %>%
-    select(rank_rating, songid, song, Estimate)
+  summary.ml.Repeatr31 <- summary(ml.Repeatr31)
 
-  mymin <- min(mydf$Estimate)
+  summary.ml.Repeatr31
 
-  mydf <- mydf %>%
-    mutate(Estimate2 = Estimate - mymin)
+  # A more detailed model that includes first song instrumental effect and potential differences between the preferences of Ian MacKaye and Guy Picciotto regarding the age of their songs.
 
-  mymax <- max(mydf$Estimate2)
+  ml.Repeatr32 <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
+                         + yearsold_6 + yearsold_7 + yearsold_8 + yearsold_1_vp + yearsold_2_vp + yearsold_3_vp + yearsold_4_vp + yearsold_5_vp + yearsold_6_vp + yearsold_7_vp + yearsold_8_vp + first_song_instrumental, data = Repeatr3)
 
-  mydf <- mydf %>%
-    mutate(rating = Estimate2/mymax)
+  summary.ml.Repeatr32 <- summary(ml.Repeatr32)
 
-  mydf <- mydf %>%
-    select(rank_rating, songid, rating)
+  summary.ml.Repeatr32
 
-  mydf2 <- read.csv("fugazi_song_performance_intensity.csv")
+  # First song model ---------------------------------------------------
 
-  mydf2$X <- NULL
+  Repeatr3_fs <- Repeatr3 %>%
+    filter(first_song==1)
 
-  mydf2 <- mydf2 %>%
-    left_join(mydf)
-
-  mydf2 <- mydf2 %>%
-    arrange(desc(rating))
-
-  mydf2 <- mydf2 %>%
-    relocate(rank_rating)
-
-  mydf3 <- read.csv("releases_songs_durations_wikipedia.csv")
-  mydf3 <- mydf3 %>% mutate(duration = seconds_to_period(duration_seconds))
-  mydf3 <- mydf3 %>% mutate(duration = sprintf('%02d:%02d', minute(duration), second(duration)))
-  mydf3 <- mydf3 %>% select(songid, duration)
-  write.csv(mydf3, "mysongdurationlookup.csv")
-
-  mydf2 <- mydf2 %>%
-    left_join(mydf3)
-
-  mydf2 <- mydf2 %>%
-    relocate(duration, .after=launchdate)
-
-  write.csv(mydf2, "summary.csv")
-
-  knitr::kable(mydf2, "pipe")
-
-
-  # Evaluation of releases using the song ratings ---------------------------
-
-  mydf <- read.csv("songs_releases.csv")
-
-  mydf <- mydf %>%
-    group_by(release) %>%
-    summarise(releaseid = mean(releaseid), releasedate=min(releasedate)) %>%
+  Repeatr3_fs_counts <- Repeatr3_fs %>%
+    filter(choice==TRUE) %>%
+    group_by(alt) %>%
+    summarise(chosen=n()) %>%
+    mutate(songid=as.integer(alt)) %>%
     ungroup()
 
-  mydf <- mydf %>%
-    select(releaseid, release, releasedate) %>%
-    arrange(releasedate)
+  Repeatr3_fs_counts <- Repeatr3_fs_counts %>%
+    left_join(mysongidlookup) %>%
+    select(alt, song, chosen)
 
-  write.csv(mydf, "releases.csv")
+  Repeatr3_fs <- Repeatr3_fs %>%
+    left_join(Repeatr3_sno_counts)
 
-  mydf <- read.csv("songs_releases.csv")
+  Repeatr3_fs <- Repeatr3_fs %>%
+    mutate(yearsold_3 = yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8)
 
-  mydf2 <- read.csv("summary.csv")
+  # It is necessary to remove the alternatives that were never chosen as the first song
 
-  mydf2 <- mydf2 %>%
-    select(songid, song, rating)
+  Repeatr3_fs <- Repeatr3_fs %>%
+    filter(is.na(chosen)==FALSE)
 
-  mydf2 <- mydf2 %>%
-    left_join(mydf)
+  ml.Repeatr3_fs <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3, data = Repeatr3_fs)
 
-  mydf2 <- mydf2 %>%
-    group_by(release, releaseid, releasedate) %>%
-    summarise(rating = mean(rating), songs_rated = n()) %>%
+  summary.ml.Repeatr3_fs <- summary(ml.Repeatr3_fs)
+
+  summary.ml.Repeatr3_fs
+
+  # Last song model ---------------------------------------------------
+
+  Repeatr3_ls <- Repeatr3 %>%
+    filter(last_song==1)
+
+  Repeatr3_ls_counts <- Repeatr3_ls %>%
+    filter(choice==TRUE) %>%
+    group_by(alt) %>%
+    summarise(chosen=n()) %>%
+    mutate(songid=as.integer(alt)) %>%
     ungroup()
 
-  mydf2 <- mydf2 %>%
-    arrange(desc(rating))
+  Repeatr3_ls_counts <- Repeatr3_ls_counts %>%
+    left_join(mysongidlookup) %>%
+    select(alt, song, chosen)
 
-  # remove First Demo as it is not comparable to the others.
-  mydf2 <- mydf2 %>%
-    filter(releaseid!=11)
+  Repeatr3_ls <- Repeatr3_ls %>%
+    left_join(Repeatr3_ls_counts)
 
-  write.csv(mydf2, "releases_rated.csv")
+  # It is necessary to remove the alternatives that were never chosen as the last song
 
-  mydf <- read.csv("releases_rated_rym.csv")
+  Repeatr3_ls <- Repeatr3_ls %>%
+    filter(is.na(chosen)==FALSE)
 
-  mydf$X <- NULL
+  ml.Repeatr3_ls <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8, data = Repeatr3_ls)
 
-  mydf <- mydf %>%
-    filter(is.na(releaseid)==FALSE)
+  summary.ml.Repeatr3_ls <- summary(ml.Repeatr3_ls)
 
-  mydf <- mydf %>%
-    select(release, releaseid, releasedate, songs_rated, rating, rym_rating)
-
-  knitr::kable(mydf, "pipe")
+  summary.ml.Repeatr3_ls
 
 }
+
+
