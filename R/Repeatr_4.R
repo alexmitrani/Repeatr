6799613e1f7a1be1cbdacca2346f33e3c2e2392c
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-01 17:10:06 -0300

    updated Repeatr_4.R

--- R/Repeatr_4.R
+++ R/Repeatr_4.R
@@ -16,6 +16,10 @@
 #' @param mydf
 #' @param savedata
 #' @param runchoicemodels
+#' @param basic_model
+#' @param first_song_model
+#' @param last_song_model
+#' @param intermediate_song_model
 #'
 #' @return
 #' @export
@@ -28,7 +32,7 @@
 #'
 #'
 
-Repeatr_4 <- function(mydf = NULL, runchoicemodels = FALSE, savedata = FALSE) {
+Repeatr_4 <- function(mydf = NULL, runchoicemodels = FALSE, basic_model = FALSE, first_song_model = FALSE, last_song_model = FALSE, intermediate_song_model = FALSE, savedata = FALSE) {
 
   # Choice modelling --------------------------------
 
@@ -49,114 +53,134 @@ Repeatr_4 <- function(mydf = NULL, runchoicemodels = FALSE, savedata = FALSE) {
 
   if (runchoicemodels == TRUE) {
 
-    # The basic model.
+    myreturnlist <- list()
 
-    ml.Repeatr4 <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
-                           + yearsold_6 + yearsold_7 + yearsold_8 , data = Repeatr4)
+    if (basic_model = TRUE) {
 
-    summary.ml.Repeatr4 <- summary(ml.Repeatr4)
+      # The basic model.
 
-    summary.ml.Repeatr4
+      ml.Repeatr4 <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
+                             + yearsold_6 + yearsold_7 + yearsold_8 , data = Repeatr4)
 
-    save(Repeatr0, Repeatr1, Repeatr2, Repeatr4, fugazi_song_counts, fugazi_song_performance_intensity, mysongidlookup, mycount, mysongvarslookup, ml.Repeatr4, file = "data.RData", compress = "xz")
+      summary.ml.Repeatr4 <- summary(ml.Repeatr4)
 
-    # First song model ---------------------------------------------------
+      summary.ml.Repeatr4
 
-    Repeatr4_fs <- Repeatr4 %>%
-      filter(first_song==1)
+      myreturnlist <- c(myreturnlist, ml.Repeatr4)
 
-    Repeatr4_fs_counts <- Repeatr4_fs %>%
-      filter(choice==TRUE) %>%
-      group_by(alt) %>%
-      summarise(chosen=n()) %>%
-      mutate(songid=as.integer(alt)) %>%
-      ungroup()
+    }
 
-    Repeatr4_fs_counts <- Repeatr4_fs_counts %>%
-      left_join(mysongidlookup) %>%
-      select(alt, song, chosen)
+    if (first_song_model = TRUE) {
 
-    Repeatr4_fs <- Repeatr4_fs %>%
-      left_join(Repeatr4_fs_counts)
+      # First song model ---------------------------------------------------
 
-    Repeatr4_fs <- Repeatr4_fs %>%
-      mutate(yearsold_3 = yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8)
+      Repeatr4_fs <- Repeatr4 %>%
+        filter(first_song==1)
 
-    # It is necessary to remove the alternatives that were never chosen
+      Repeatr4_fs_counts <- Repeatr4_fs %>%
+        filter(choice==TRUE) %>%
+        group_by(alt) %>%
+        summarise(chosen=n()) %>%
+        mutate(songid=as.integer(alt)) %>%
+        ungroup()
 
-    Repeatr4_fs <- Repeatr4_fs %>%
-      filter(is.na(chosen)==FALSE)
+      Repeatr4_fs_counts <- Repeatr4_fs_counts %>%
+        left_join(mysongidlookup) %>%
+        select(alt, song, chosen)
 
-    ml.Repeatr4_fs <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3, data = Repeatr4_fs)
+      Repeatr4_fs <- Repeatr4_fs %>%
+        left_join(Repeatr4_fs_counts)
 
-    summary.ml.Repeatr4_fs <- summary(ml.Repeatr4_fs)
+      Repeatr4_fs <- Repeatr4_fs %>%
+        mutate(yearsold_3 = yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8)
 
-    summary.ml.Repeatr4_fs
+      # It is necessary to remove the alternatives that were never chosen
 
-    save(Repeatr0, Repeatr1, Repeatr2, Repeatr4, fugazi_song_counts, fugazi_song_performance_intensity, mysongidlookup, mycount, mysongvarslookup, ml.Repeatr4, ml.Repeatr4_fs, file = "data.RData", compress = "xz")
+      Repeatr4_fs <- Repeatr4_fs %>%
+        filter(is.na(chosen)==FALSE)
 
-    # Last song model ---------------------------------------------------
+      ml.Repeatr4_fs <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3, data = Repeatr4_fs)
 
-    Repeatr4_ls <- Repeatr4 %>%
-      filter(last_song==1)
+      summary.ml.Repeatr4_fs <- summary(ml.Repeatr4_fs)
 
-    Repeatr4_ls_counts <- Repeatr4_ls %>%
-      filter(choice==TRUE) %>%
-      group_by(alt) %>%
-      summarise(chosen=n()) %>%
-      mutate(songid=as.integer(alt)) %>%
-      ungroup()
+      summary.ml.Repeatr4_fs
 
-    Repeatr4_ls_counts <- Repeatr4_ls_counts %>%
-      left_join(mysongidlookup) %>%
-      select(alt, song, chosen)
+      myreturnlist <- c(myreturnlist, ml.Repeatr4_fs)
 
-    Repeatr4_ls <- Repeatr4_ls %>%
-      left_join(Repeatr4_ls_counts)
+    }
 
-    # It is necessary to remove the alternatives that were never chosen
+    if (last_song_model = TRUE) {
 
-    Repeatr4_ls <- Repeatr4_ls %>%
-      filter(is.na(chosen)==FALSE)
+      # Last song model ---------------------------------------------------
 
-    ml.Repeatr4_ls <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8, data = Repeatr4_ls)
+      Repeatr4_ls <- Repeatr4 %>%
+        filter(last_song==1)
 
-    summary.ml.Repeatr4_ls <- summary(ml.Repeatr4_ls)
+      Repeatr4_ls_counts <- Repeatr4_ls %>%
+        filter(choice==TRUE) %>%
+        group_by(alt) %>%
+        summarise(chosen=n()) %>%
+        mutate(songid=as.integer(alt)) %>%
+        ungroup()
 
-    summary.ml.Repeatr4_ls
+      Repeatr4_ls_counts <- Repeatr4_ls_counts %>%
+        left_join(mysongidlookup) %>%
+        select(alt, song, chosen)
 
-    save(Repeatr0, Repeatr1, Repeatr2, Repeatr4, fugazi_song_counts, fugazi_song_performance_intensity, mysongidlookup, mycount, mysongvarslookup, ml.Repeatr4, ml.Repeatr4_fs, ml.Repeatr4_ls, file = "data.RData", compress = "xz")
+      Repeatr4_ls <- Repeatr4_ls %>%
+        left_join(Repeatr4_ls_counts)
 
-  # Intermediate song model -------------------------------------------------
+      # It is necessary to remove the alternatives that were never chosen
 
-    Repeatr4_is <- Repeatr4 %>%
-      filter(first_song==0 & last_song==0)
+      Repeatr4_ls <- Repeatr4_ls %>%
+        filter(is.na(chosen)==FALSE)
 
-    Repeatr4_is_counts <- Repeatr4_is %>%
-      filter(choice==TRUE) %>%
-      group_by(alt) %>%
-      summarise(chosen=n()) %>%
-      mutate(songid=as.integer(alt)) %>%
-      ungroup()
+      ml.Repeatr4_ls <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5 + yearsold_6 + yearsold_7 + yearsold_8, data = Repeatr4_ls)
 
-    Repeatr4_is_counts <- Repeatr4_is_counts %>%
-      left_join(mysongidlookup) %>%
-      select(alt, song, chosen)
+      summary.ml.Repeatr4_ls <- summary(ml.Repeatr4_ls)
 
-    Repeatr4_is <- Repeatr4_is %>%
-      left_join(Repeatr4_is_counts)
+      summary.ml.Repeatr4_ls
 
-    # It is necessary to remove the alternatives that were never chosen
+      myreturnlist <- c(myreturnlist, ml.Repeatr4_ls)
 
-    Repeatr4_is <- Repeatr4_is %>%
-      filter(is.na(chosen)==FALSE)
+    }
 
-    ml.Repeatr4_is <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
-                           + yearsold_6 + yearsold_7 + yearsold_8 + yearsold_1_vp + yearsold_2_vp + yearsold_3_vp + yearsold_4_vp + yearsold_5_vp + yearsold_6_vp + yearsold_7_vp + yearsold_8_vp, data = Repeatr4_is)
+    if (intermediate_song_model = TRUE) {
 
-    summary.ml.Repeatr4_is <- summary(ml.Repeatr4_is)
+      # Intermediate song model -------------------------------------------------
 
-    summary.ml.Repeatr4_is
+      Repeatr4_is <- Repeatr4 %>%
+        filter(first_song==0 & last_song==0)
+
+      Repeatr4_is_counts <- Repeatr4_is %>%
+        filter(choice==TRUE) %>%
+        group_by(alt) %>%
+        summarise(chosen=n()) %>%
+        mutate(songid=as.integer(alt)) %>%
+        ungroup()
+
+      Repeatr4_is_counts <- Repeatr4_is_counts %>%
+        left_join(mysongidlookup) %>%
+        select(alt, song, chosen)
+
+      Repeatr4_is <- Repeatr4_is %>%
+        left_join(Repeatr4_is_counts)
+
+      # It is necessary to remove the alternatives that were never chosen
+
+      Repeatr4_is <- Repeatr4_is %>%
+        filter(is.na(chosen)==FALSE)
+
+      ml.Repeatr4_is <- mlogit(choice ~ yearsold_1 + yearsold_2 + yearsold_3 + yearsold_4 + yearsold_5
+                             + yearsold_6 + yearsold_7 + yearsold_8 + yearsold_1_vp + yearsold_2_vp + yearsold_3_vp + yearsold_4_vp + yearsold_5_vp + yearsold_6_vp + yearsold_7_vp + yearsold_8_vp, data = Repeatr4_is)
+
+      summary.ml.Repeatr4_is <- summary(ml.Repeatr4_is)
+
+      summary.ml.Repeatr4_is
+
+      myreturnlist <- c(myreturnlist, ml.Repeatr4_is)
+
+    }
 
   }
 
@@ -167,8 +191,6 @@ Repeatr_4 <- function(mydf = NULL, runchoicemodels = FALSE, savedata = FALSE) {
     save(Repeatr0, Repeatr1, Repeatr2, Repeatr3, Repeatr4, fugazi_song_counts, fugazi_song_performance_intensity, mysongidlookup, mycount, mysongvarslookup, releasesdatalookup, ml.Repeatr4, ml.Repeatr4_fs, ml.Repeatr4_ls, ml.Repeatr4_is, file = "data.RData", compress = "xz")
 
   }
-
-  myreturnlist <- list(ml.Repeatr4, ml.Repeatr4_fs, ml.Repeatr4_ls, ml.Repeatr4_is)
 
   return(myreturnlist)
 
