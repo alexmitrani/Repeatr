d82b280e343fcf05de193c92a4a8d5e04daf61d2
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-01 15:20:38 -0300

    Update compressr.R

--- R/compressr.R
+++ R/compressr.R
@@ -7,7 +7,7 @@
 #' @description compressr is used to reduce the size of data files with double-precision storage of integer variables, by changing the storage type of these variables to integer.
 #' @details compressr is used internally by the fsm package.
 #'
-#' @import tidyverse
+#' @import dplyr
 #' @import crayon
 #' @import rlang
 #'
