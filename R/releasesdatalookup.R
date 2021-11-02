82db2165f0046f4de5eb72a466c76588d3558e89
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-02 07:44:39 -0300

    Create releasesdatalookup.R

---
+++ R/releasesdatalookup.R
@@ -0,0 +1,15 @@
+#' Fugazi releases data
+#'
+#' This data was scraped from the Fugazi Live Series website by Carni Klirs for his project "Visualizing the History of Fugazi".
+#'
+#' @source https://web.archive.org/web/20210211085323/https://rateyourmusic.com/artist/fugazi
+#' @format dataframe with one release per row.
+#' \describe{
+#' \item{release}{release name}
+#' \item{releaseid}{numeric id in ascending chronological order}
+#' \item{releasedate}{release date}
+#' \item{rym_rating}{RYM rating scaled to the interval [0,1]}
+#' }
+#' @examples
+#'   releasesdatalookup
+"releasesdatalookup"
