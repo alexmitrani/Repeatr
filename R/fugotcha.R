fe338ba013dfcfca7a2e7fbe911610662b216d51
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-01 23:10:16 -0300

    Create fugotcha.R

---
+++ R/fugotcha.R
@@ -0,0 +1,23 @@
+#' Fugazi Live Series data
+#'
+#' This data was scraped from the Fugazi Live Series website by Carni Klirs for his project "Visualizing the History of Fugazi".
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format CSV file with one show per row.
+#' \describe{
+#' \item{V1}{show id}
+#' \item{V2}{Fugazi Live Series id}
+#' \item{V3}{Show Date}
+#' \item{V4}{Venue}
+#' \item{V5}{Door Price}
+#' \item{V6}{Attendance}
+#' \item{V7}{Recorded by}
+#' \item{V8}{Mastered by}
+#' \item{V9}{Original source}
+#' \item{V10-V50}{Tracks}
+
+#' }
+#' @examples
+#'   fugotcha <- "./inst/extdata/fugotcha.csv"
+#'   mydf <- read.csv(fugotcha, header=FALSE)
+"fugotcha.csv"
