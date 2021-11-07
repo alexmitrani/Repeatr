a8f415adf98772adbc55b0aa9893016d34956c4d
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-07 14:54:28 -0300

    Update rankr.R

--- R/rankr.R
+++ R/rankr.R
@@ -51,14 +51,16 @@ rankr <- function(mymodel = NULL, fromcoef = NULL, tocoef = NULL) {
     rename(song2 = song)
 
   myresultsdf <- myresultsdf %>%
-    left_join(songidlookup1) %>%
+    left_join(songidlookup1)
+
+  myresultsdf <- myresultsdf %>%
     left_join(songidlookup2) %>%
 
   myresultsdf <- myresultsdf %>%
     relocate(song1, song2)
 
-  myresultsdf <- myresultsdf %>%
-    select(song1, song2, mycoef1, mycoef2, mycoefdiff, myz, myp, lower95ci, upper95ci)
+# myresultsdf <- myresultsdf %>%
+ #   select(song1, song2, mycoef1, mycoef2, mycoefdiff, myz, myp, lower95ci, upper95ci)
 
   return(myresultsdf)
 
