97deb2657c3e7e1d797aa2c25574bd491b0349c5
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-15 09:08:29 -0300

    updated index.Rmd and render_index.R

--- R/render_index.R
+++ R/render_index.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 code_dir <- "vignettes"
 index_filename <- "index.Rmd"
-index_filename <- file.path(code_dir, report_filename)
+index_filename <- file.path(code_dir, index_filename)
 output_dir <- "docs"
 output <- file.path("..",output_dir)
 render(index_filename, output_dir = output_dir, params = list(output_dir = output))
