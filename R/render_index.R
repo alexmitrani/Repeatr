2b7ee4b4609634eb7c7c4744b1ffa7b45b2740ed
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-11-15 10:13:26 -0300

    Moved the render process to Render_index.Rmd

--- R/render_index.R
+++
@@ -1,8 +0,0 @@
-library(rmarkdown)
-code_dir <- "vignettes"
-index_filename <- "index.Rmd"
-index_filename <- file.path(code_dir, index_filename)
-output_dir <- "docs"
-output <- file.path("..",output_dir)
-render(index_filename, output_dir = output_dir, params = list(output_dir = output))
-
