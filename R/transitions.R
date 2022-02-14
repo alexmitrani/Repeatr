2dab3d4932bbfcadaf4e59fc9575139018a736aa
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-02-13 22:17:13 -0300

    Update transitions.R

--- R/transitions.R
+++ R/transitions.R
@@ -3,6 +3,8 @@ library(ggraph)
 library(igraph)
 library(dplyr)
 
+load("~/Documents/GitHub/Repeatr/data/Repeatr1.rda")
+
 mydf1 <- Repeatr1 %>% select(gid,song_number,song)
 mydf2 <- mydf1 %>% mutate(song_number = song_number-1)
 mydf1 <- mydf1 %>% rename(song1 = song)
@@ -11,5 +13,12 @@ mydf3 <- mydf1 %>% left_join(mydf2)
 mydf3 <- mydf3 %>% filter(is.na(song2)==FALSE)
 mydf3 <- mydf3 %>% rename(transition_number = song_number)
 head(mydf3)
+
+load("~/Documents/GitHub/Repeatr/data/songvarslookup.rda")
+load("~/Documents/GitHub/Repeatr/data/songidlookup.rda")
+songvars <- songvarslookup %>% left_join(songidlookup)
+songvars <- songvars %>% select(releaseid,release,track_number,song)
+songvars <- songvars %>% arrange(releaseid,track_number)
+
 
 
