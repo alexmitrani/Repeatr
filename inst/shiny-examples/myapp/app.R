d8691ac238c428c36274ded029e65c8a6650a0f2
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-08-15 20:30:48 -0400

    Update app.R

---
+++ inst/shiny-examples/myapp/app.R
@@ -0,0 +1,75 @@
+
+library(shiny)
+library(Repeatr)
+
+
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("Hello Shiny!"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+
+    )
+  )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+
+    # What is the total number of people that Fugazi performed for in the shows that are available in the Fugazi Live Series data?
+    test <- Repeatr0
+    test <- test %>% mutate(attendancedata = nchar(V6))
+    test <- test %>% filter(attendancedata>0)
+    test <- test %>% mutate(attendance = as.numeric(V6))
+    test <- test %>% filter(is.na(attendance)==FALSE)
+    test <- test %>% select(attendance)
+
+    x    <- test$attendance
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Attendances of Fugazi shows (people)",
+         main = "Histogram of attendance at Fugazi shows")
+
+  })
+
+}
+
+
+shinyApp(ui = ui, server = server)
+
+
+#
+
