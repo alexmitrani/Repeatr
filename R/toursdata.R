24df5b560a1b9016d159279d3d26aa2690c80bc7
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-08-28 22:28:38 -0400

    Create toursdata.R

---
+++ R/toursdata.R
@@ -0,0 +1,19 @@
+#' Tours data
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format dataframe with one row for each tour in the Fugazi Live Series data.
+#' \describe{
+#' \item{tour}{Name of the tour}
+#' \item{start}{Start date}
+#' \item{end}{End date}
+#' \item{shows}{Number of shows}
+#' \item{duration}{Duration of the tour in days}
+#' \item{attendance}{Total attendance of all the shows}
+#' \item{meanattendance}{Average attendance per show}
+#' \item{startyear}{Year in which the tour started}
+#' \item{endyear}{Year in which the tour ended}
+#'
+#' }
+#' @examples
+#'   toursdata
+"toursdata"
