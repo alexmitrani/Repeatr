a742dcc31e6d6c131f41bd1abc4fc8eb3f6826d2
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-08-28 22:31:00 -0400

    Create attendancedata.R

---
+++ R/attendancedata.R
@@ -0,0 +1,15 @@
+#' Attendance data
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format dataframe with one row for each show for which attendance was recorded in the Fugazi Live Series data.
+#' \describe{
+#' \item{year}{Year of the show}
+#' \item{date}{Date of the show}
+#' \item{venue}{Venue of the show}
+#' \item{attendance}{Estimated attendance}
+#'
+#'
+#' }
+#' @examples
+#'   attendancedata
+"attendancedata"
