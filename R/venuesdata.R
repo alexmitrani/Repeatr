a0928625e87b0b9a725168da6107009a905331cc
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-11-06 19:43:44 -0300

    Create venuesdata.R

---
+++ R/venuesdata.R
@@ -0,0 +1,15 @@
+#' Venues data
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format dataframe with one row for each venue in the Fugazi Live Series data.
+#' \describe{
+#' \item{venue}{Venue of the show}
+#' \item{city}{City}
+#' \item{country}{Country}
+#' \item{from}{Year of first show at the venue}
+#' \item{to}{Year of last show at the venue}
+#'
+#' }
+#' @examples
+#'   venuesdata
+"venuesdata"
