2a10771b90dda8383eaf6c281e5affde3656816b
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-11-24 00:26:13 -0300

    Updating site for new version of Repeatr package.

---
+++ R/cumulative_song_counts.R
@@ -0,0 +1,15 @@
+#' Cumulative Song Counts
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format dataframe with one row for song performance in the Fugazi Live Series data.
+#' \describe{
+#' \item{date}{Date of the show}
+#' \item{song}{Name of the song}
+#' \item{release}{Name of the corresponding discographical release}
+#' \item{count}{The cumulative count of the number of times the song had been performed up to and including this performance.}
+#'
+#'
+#' }
+#' @examples
+#'   cumulative_song_counts
+"cumulative_song_counts"
