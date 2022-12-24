d60ba3732c4c161940328419f5f6e957f5843ed6
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-12-24 19:16:36 -0300

    Update Repeatr.R

--- inst/shiny/Repeatr-app/Repeatr.R
+++ inst/shiny/Repeatr-app/Repeatr.R
@@ -247,7 +247,8 @@ server <- function(input, output, session) {
     if(input$yearInput_shows=="All") {
       othervariables
     } else {
-      filter(othervariables, year == input$yearInput_shows)
+      othervariables %>%
+        filter(year == input$yearInput_shows)
     }
   })
 
@@ -261,7 +262,8 @@ server <- function(input, output, session) {
     if(input$tourInput_shows=="All") {
       year_data()
     } else {
-      filter(year_data(), tour == input$tourInput_shows)
+      year_data() %>%
+        filter(tour == input$tourInput_shows)
     }
 
   })
@@ -276,7 +278,8 @@ server <- function(input, output, session) {
     if(input$countryInput_shows=="All") {
       tour_data()
     } else {
-      filter(tour_data(), country == input$countryInput_shows)
+      tour_data() %>%
+      filter(country == input$countryInput_shows)
     }
   })
 
@@ -290,27 +293,80 @@ server <- function(input, output, session) {
     if(input$cityInput_shows=="All") {
       country_data()
     } else {
-      filter(country_data(), city == input$cityInput_shows)
+      country_data() %>%
+      filter(city == input$cityInput_shows)
     }
+  })
+
+  output_data <- reactive({
+
+    city_data()%>%
+      filter(is.na(attendance)==FALSE) %>%
+      filter(is.na(tour)==FALSE) %>%
+      filter(date >= input$dateInput_shows[1] &
+               date <= input$dateInput_shows[2]) %>%
+      mutate(attendance = as.integer(attendance)) %>%
+      mutate(date = as.Date(date, "%d-%m-%Y")) %>%
+      mutate(year = lubridate::year(date)) %>%
+      rename(latitude = y) %>%
+      rename(longitude = x) %>%
+      select(flsid, tour, year, date, venue, city, country, attendance, doorprice, latitude, longitude, checked) %>%
+      rename(door_price = doorprice,
+             fls_id = flsid)
+
   })
 
   output$data <- DT::renderDataTable(DT::datatable({
 
-      city_data()%>%
-        filter(is.na(attendance)==FALSE) %>%
-        filter(is.na(tour)==FALSE) %>%
-        filter(date >= input$dateInput_shows[1] &
-                 date <= input$dateInput_shows[2]) %>%
-        mutate(attendance = as.integer(attendance)) %>%
-        mutate(date = as.Date(date, "%d-%m-%Y")) %>%
-        mutate(year = lubridate::year(date)) %>%
-        rename(latitude = y) %>%
-        rename(longitude = x) %>%
-        select(flsid, tour, year, date, venue, city, country, attendance, doorprice, latitude, longitude, checked) %>%
-        rename(door_price = doorprice,
-               fls_id = flsid)
+    output_data()
+
     }
   ))
+
+  output$mymap <- renderLeaflet({
+    df <- output_data()
+
+    ref_latitude <- mean(df$latitude)
+    ref_longitude <- mean(df$longitude)
+
+    min_latitude_raw <- min(df$latitude)
+    min_longitude_raw <- min(df$longitude)
+
+    max_latitude_raw <- max(df$latitude)
+    max_longitude_raw <- max(df$longitude)
+
+    diff_longitude <- abs(max_longitude_raw-min_longitude_raw)
+    diff_latitude <- abs(max_latitude_raw-min_latitude_raw)
+
+    diff <- mean(diff_longitude, diff_latitude)
+    margin_value <- ifelse(diff==0, 0.1, 0.1*diff)
+
+    min_latitude <- min(df$latitude)-margin_value
+    min_longitude <- min(df$longitude)-margin_value
+
+    max_latitude <- max(df$latitude)+margin_value
+    max_longitude <- max(df$longitude)+margin_value
+
+    m <- leaflet(data = df) %>%
+      fitBounds(lng1 = min_longitude, lat1 = min_latitude, lng2 = max_longitude, lat2 = max_latitude) %>%
+      addProviderTiles("Esri.WorldStreetMap") %>%
+      addScaleBar() %>%
+      addCircles(
+        data = df,
+        radius = sqrt((df$attendance)/pi),
+        color = "#F60D1D",
+        fillColor = "#F60D1D",
+        fillOpacity = 0.5,
+        popup = paste0(
+          "<strong>Date: </strong>", df$date, "<br>",
+          "<strong>Venue: </strong>", df$venue, "<br>",
+          "<strong>City: </strong>", df$city, "<br>",
+          "<strong>Attendance: </strong>", df$attendance, "<br>"
+        )
+      )
+    m
+
+  })
 
 }
 
