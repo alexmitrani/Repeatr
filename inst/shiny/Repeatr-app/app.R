2f56da8279deb80b056ececda71ad32f3364ed9e
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2022-12-25 03:08:48 -0300

    Update app.R

--- inst/shiny/Repeatr-app/app.R
+++ inst/shiny/Repeatr-app/app.R
@@ -21,6 +21,7 @@ shows_data <- othervariables %>%
 
 ui <- fluidPage(
 
+  tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
 
   h1("Repeatr"),
   tags$div(
@@ -67,7 +68,7 @@ ui <- fluidPage(
                              fluidRow(
 
 
-                               leafletOutput("mymap", height = 500, width = 500)
+                               leafletOutput("mymap")
 
 
                              ),
