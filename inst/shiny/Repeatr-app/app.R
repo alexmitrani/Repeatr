c1ab6e3982d09ce3c9cbca013dd166d131552e17
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-01-09 01:16:03 -0300

    Update app.R

--- inst/shiny/Repeatr-app/app.R
+++ inst/shiny/Repeatr-app/app.R
@@ -22,7 +22,6 @@ shows_data <- othervariables %>%
 ui <- fluidPage(
 
   tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
-  tags$style("#weeks {font-size:10px;}"),
 
   h1("Repeatr"),
 
