c9705db6ffd4552fcacccd19175551432dcbf8b6
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-01-31 00:05:21 -0300

    fix for #65

--- R/Repeatr_1.R
+++ R/Repeatr_1.R
@@ -25,7 +25,7 @@
 #' @examples
 #' fugotcha <- system.file("extdata", "fugotcha.csv", package = "Repeatr")
 #' releases_songs_durations_wikipedia <- system.file("extdata", "releases_songs_durations_wikipedia.csv", package = "Repeatr")
-#' releasesdatafile <- system.file("extdata", "releases_rym.csv", package = "Repeatr")
+#' releasesdatafile <- system.file("extdata", "releases.csv", package = "Repeatr")
 #' Repeatr_1_results <- Repeatr_1(mycsvfile = fugotcha, mysongdatafile = releases_songs_durations_wikipedia, releasesdatafile = releasesdatafile)
 #'
 Repeatr_1 <- function(mycsvfile = NULL, mysongdatafile = NULL, releasesdatafile = NULL) {
@@ -86,7 +86,7 @@ Repeatr_1 <- function(mycsvfile = NULL, mysongdatafile = NULL, releasesdatafile 
 
   } else {
 
-    releasesdatafile <- system.file("extdata", "releases_rym.csv", package = "Repeatr")
+    releasesdatafile <- system.file("extdata", "releases.csv", package = "Repeatr")
     releasesdatalookup <- read.csv(releasesdatafile)
     releasesdatalookup$X <- NULL
 
