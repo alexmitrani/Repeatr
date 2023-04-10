5e08d29862a43d5635dd895926f8506e7c519472
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-04-09 21:08:06 -0400

    added fls_tags.rda to the package

---
+++ R/fls_tags.R
@@ -0,0 +1,14 @@
+#' Tags data
+#'
+#' @source https://www.dischord.com/fugazi_live_series
+#' @format dataframe with one row for each track in the Fugazi Live Series data, including data from the audio file tags.
+#' \describe{
+#' \item{track}{track number}
+#' \item{album}{album name, which includes the date, venue, city, state, and country}
+#' \item{name}{track name}
+#' \item{duration}{duration in time format}
+#'
+#' }
+#' @examples
+#'   fls_tags
+"fls_tags"
