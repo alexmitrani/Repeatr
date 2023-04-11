9d67e6beceb76da59b5f5e1f340a1c5cb05fe4f8
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-04-10 23:31:01 -0400

    Update fls_tags_importer.R

--- R/fls_tags_importer.R
+++ R/fls_tags_importer.R
@@ -3,7 +3,6 @@
 #' @name fls_tags_importer
 #' @title imports a CSV file of duration data, converts the duration variable to hh:mm:ss (hms) format, and exports the resulting data to an rda file.
 #' @description fls_tags_importer is used to import a CSV file of duration data generated with kid3 audio tagger (https://kid3.kde.org/)
-#' @details fls_tags_importer is used internally by the Repeatr package.
 #'
 #'
 #' @param myfilename the full path and filename of the file to be imported and converted.
