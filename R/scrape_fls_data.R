455bdb7ae97ee04c5ae983d6ba87fda3683a5a2a
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-09-20 00:41:35 -0300

    added code and data for #113

--- R/scrape_fls_data.R
+++ R/scrape_fls_data.R
@@ -1,31 +1,48 @@
 
 
-#' @name scrape_sound_quality
-#' @title Scrape Sound Quality ratings from Fugazi Live Series pages
-#' @description A simple function to scrape sound quality ratings from the Fugazi Live Series website using rvest.
+#' @name scrape_fls_data
+#' @title Scrape data from Fugazi Live Series pages
+#' @description A simple function to scrape data from the Fugazi Live Series website using rvest.
 #'
+#' @param mygiddata Name of data frame containing list of gids to be scraped (optional).  If this is not specified gids from othervariables will be used (1048 of them).
 #' @param mylimit The number of shows from which to scrape sound quality rating. Set to a low number for testing.
 #' @param mycsvfilename filename for the CSV file to which the results will be written.
 #' @param sleepseconds seconds to wait before getting info from the next page.
+#' @param my_data_html_element html element that contains the data to be scraped
 #'
 #' @import rvest
 #' @return
 #' @export
 #'
 #' @examples
-#' mydf <- scrape_sound_quality(mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_sound_quality.csv")
+#' scraped_data_1 <- scrape_fls_data(mygiddata = NULL, mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_sound_quality.csv", my_data_html_element = "dd strong")
+#' scraped_data_2 <- scrape_fls_data(mygiddata = NULL, mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_played_with.csv", my_data_html_element = "dd:nth-child(10)")
+#' mydf <- system.file("extdata", "gid_played_with_8.csv", package = "Repeatr")
+#' scraped_data_3 <- scrape_fls_data(mygiddata = mydf, mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_played_with_8.csv", my_data_html_element = "dd:nth-child(8)")
+#' mydf <- system.file("extdata", "gid_played_with_6.csv", package = "Repeatr")
+#' scraped_data_4 <- scrape_fls_data(mygiddata = mydf, mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_played_with_6.csv", my_data_html_element = "dd:nth-child(6)")
 #'
-scrape_sound_quality <- function(mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_sound_quality.csv") {
+scrape_fls_data <- function(mygiddata = NULL, mylimit = 3, sleepseconds = 1, mycsvfilename = "gid_fls_id_sound_quality.csv", my_data_html_element = "dd strong") {
 
-  gid_url <- fls_tags_show %>%
-    select(gid) %>%
-    mutate(url = paste0("https://www.dischord.com/fugazi_live_series/", gid))
+  if (is.null(mygiddata) == TRUE) {
+
+    gid_url <- othervariables %>%
+      select(gid) %>%
+      mutate(url = paste0("https://www.dischord.com/fugazi_live_series/", gid))
+
+  } else {
+
+    gid_url <- mygiddata %>%
+      select(gid) %>%
+      mutate(url = paste0("https://www.dischord.com/fugazi_live_series/", gid))
+
+  }
 
   httr::set_config(httr::user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"))
 
   # initializing the lists that will store the scraped data
   fls_id <- list()
-  sound_quality <- list()
+  fls_data <- list()
 
   # initializing the list of pages to scrape with the first pagination links
   pages_to_scrape <- as.data.frame(gid_url$url)
@@ -69,10 +86,10 @@ scrape_sound_quality <- function(mylimit = 3, sleepseconds = 1, mycsvfilename = 
         html_text2()
     )
 
-    sound_quality <- c(
-      sound_quality,
+    fls_data <- c(
+      fls_data,
       html_show %>%
-        html_element("dd strong") %>%
+        html_element(my_data_html_element) %>%
         html_text2()
     )
 
@@ -85,26 +102,27 @@ scrape_sound_quality <- function(mylimit = 3, sleepseconds = 1, mycsvfilename = 
   }
 
   # converting the lists containg the scraped data into a data.frame
-  fls_id_sound_quality <- data.frame(
+  fls_id_data <- data.frame(
     unlist(fls_id),
-    unlist(sound_quality)
+    unlist(fls_data)
   )
 
   # changing the column names of the data frame before exporting it into CSV
-  names(fls_id_sound_quality) <- c("fls_id", "sound_quality")
+  names(fls_id_data) <- c("fls_id", "fls_data")
 
   gid <- as.data.frame(gid_url$gid)
   gid <- gid[1:limit, ]
 
   # results including gid
-  results <- cbind.data.frame(gid, fls_id_sound_quality)
+  results <- cbind.data.frame(gid, fls_id_data)
 
   results <- results %>%
-    mutate(fls_id = str_replace(fls_id, "Fugazi Live Series ", ""))
+    mutate(fls_id = str_replace(fls_id, "Fugazi Live Series ", ""),
+           fls_data = str_trim(fls_data))
 
   # export the data frame containing the scraped data to a CSV file
   filename <- paste0("./", mycsvfilename)
-  write.csv(results, file = "./gid_fls_id_sound_quality.csv", fileEncoding = "UTF-8", row.names = FALSE)
+  write.csv(results, file = filename, fileEncoding = "UTF-8", row.names = FALSE)
 
   return(results)
 
